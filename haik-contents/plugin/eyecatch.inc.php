4e2dbe4a51675d321995b18727d6e2568f1f2e3e
hokuken <customer@hokuken.com>
2014-01-07 18:55:51 +0900

    eyecatch 内であることを判別するためのグローバル変数を追加

--- haik-contents/plugin/eyecatch.inc.php
+++ haik-contents/plugin/eyecatch.inc.php
@@ -98,14 +98,21 @@ function plugin_eyecatch_action()
 
 function plugin_eyecatch_convert()
 {
+	// アイキャッチのslideプラグインで判別するため
+	// true: タイトルがh1に、false: タイトルがh3に
+	global $is_eyecatch; 
+
 	$args = func_get_args();
-	
+
+
 	$html = '';
+	$is_eyecatch = TRUE;
 	if (exist_plugin('section'))
 	{
 		array_unshift($args, 'eyecatch');
 		$html = call_user_func_array('plugin_section_convert', $args);
 	}
+	$is_eyecatch = FALSE;
 	
 	$qt = get_qt();
 	$qt->setv('eyecatch', $html);
