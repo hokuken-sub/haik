283fcbe3f9e25bd3b2e001b7f70a0e27fd448b3b
hokuken <customer@hokuken.com>
2014-01-23 19:12:09 +0900

    v0.14.0-RC

--- haik-contents/lib/init.php
+++ haik-contents/lib/init.php
@@ -18,7 +18,7 @@
 
 define('APP_NAME', 'haik');
 define('S_VERSION', '1.4.7');//PukiWiki version
-define('APP_VERSION', '0.13.0-RC');
+define('APP_VERSION', '0.14.0-RC');
 
 // URLs
 define('APP_OFFICIAL_SITE', 'http://toiee.jp/haik/');
