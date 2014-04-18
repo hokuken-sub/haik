d29284b169722e7a7e2382a221e3660f0ab306e6
masamori <r.masamori@gmail.com>
2014-04-18 10:03:19 +0900

    Fix convert body

--- haik-contents/plugin/menu_stacked.inc.php
+++ haik-contents/plugin/menu_stacked.inc.php
@@ -18,54 +18,51 @@
  */
 function plugin_menu_stacked_convert()
 {
-	global $vars, $script;
-	
-	$qt = get_qt();
-	
-	$args = func_get_args();
-	$body = trim(array_pop($args));
-	
+    global $vars, $script;
+    $qt = get_qt();
+
+    $args = func_get_args();
+    $body = trim(array_pop($args));
     $body = str_replace("\r", "\n", str_replace("\r\n", "\n", $body));
-	$lines = explode("\n", $body);
+    $body = convert_html($body);
 
-	$body = convert_html($lines);
-	
-	$the_url = get_page_url($vars['page']);
+    $the_url = get_page_url($vars['page']);
 
-	$regx = array(
-		'/<ul class="list1"/',
-		'/<ul class="list[2-3]"/',
-		'/<li>(<a href="'. h(preg_quote($the_url, '/')) .'")/',
-		'/<li>(<a [^>]+)/',
-		'/<\/li>/',
-		'/<\/ul>/',
-	);
-	$replace = array(
-		'<div class="list-group"',
-		'<div /',
-		'\1 class="list-group-item active"',
-		'\1 class="list-group-item"',
-		'',
-		'</div>',
-	);
-	$body = preg_replace($regx, $replace, $body);
-	
-	$addscript = '
+    $regx = array(
+        '/<ul class="list1"/',
+        '/<ul class="list[2-3]"/',
+        '/<li>(<a href="'. h(preg_quote($the_url, '/')) .'")/',
+        '/<li>(<a [^>]+)/',
+        '/<\/li>/',
+        '/<\/ul>/',
+    );
+    $replace = array(
+        '<div class="list-group"',
+        '<div /',
+        '\1 class="list-group-item active"',
+        '\1 class="list-group-item"',
+        '',
+        '</div>',
+    );
+    $body = preg_replace($regx, $replace, $body);
+
+    $addscript = '
 <script type="text/javascript">
 $(function(){
-	$("#orgm_menu ul.nav-stacked").each(function(){
-		var $lis = $("> li", this);
-		
-		$lis.each(function(){
-			$("> a", this).prepend("<i class=\"icon-chevron-right pull-right\"></i>");
-		});
+$("#orgm_menu ul.nav-stacked").each(function(){
+	var $lis = $("> li", this);
+	
+	$lis.each(function(){
+		$("> a", this).prepend("<i class=\"icon-chevron-right pull-right\"></i>");
 	});
+});
 
 });
 </script>
 ';
-	$qt->appendv_once('plugin_menu_stacked', 'plugin_script', $addscript);
+    $qt->appendv_once('plugin_menu_stacked', 'plugin_script', $addscript);
 
-	return $body;
+    return $body;
 }
+
 ?>
